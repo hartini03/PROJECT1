b41d16b601a289e335c19e16cfba93bab9396179
hartini03 <hartiniattari03@gmail.com>
2021-09-03 12:59:44 +0700

    first commit

---
+++ src/Percabangan.java
@@ -0,0 +1,18 @@
+public class Percabangan {
+
+
+    public static void main (String[] args){
+
+        int y=2;
+        switch (y){
+            case 1:
+                System.out.println("nilai y = 1");
+                break;
+            case 2:
+                System.out.println("nilai y = 2");
+                break;
+            default:
+                System.out.println("nilai a tidak diketahui");
+        }
+    }
+}
